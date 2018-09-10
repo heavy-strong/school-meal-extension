aa60c53035fe81a5fcf5d07b2ef546853dc1599a
Unknown <jrady721@gmail.com>
2018-09-10 10:01:54 +0900

    bug fix

--- background.js
+++ background.js
@@ -1,84 +1,105 @@
-chrome.runtime.onInstalled.addListener(function () {
-    $(function () {
-        let keys = ['office', 'school', 'level'];
-        chrome.storage.local.get(keys, function (data) {
-            if (!data.office) {
-                console.log('1');
-                let setting = [{office: 'dge.go.kr'}, {school: 'D100000282'}, {level: '4'}]
-                setting.forEach(function (data2) {
-                    chrome.storage.local.set(data2, function () {
-                        getMenu(getYmd(new Date()), data)
-                    })
-                });
-            } else {
-                getMenu(getYmd(new Date()), data)
-            }
-        })
-    })
-});
+const keys = ['office', 'school', 'level']
+const baseURL = 'http://jrady721.cafe24.com'
+// const baseURL = 'http://115.68.22.74:3000';
 
-function getMenu(ymd, data) {
-    console.log(data);
-    $.get(`http://jrady721.cafe24.com/api/meal/${ymd}/type/1/office/${data.office}/school/${data.school}/level/${data.level}`, function (data) {
+// 처음 설치 후 실행
+chrome.runtime.onInstalled.addListener(function () {
+
+})
+
+// 백그라운드
+chrome.storage.local.get(keys, function (setting) {
+    // 설정이 존재하지 않으면 기본설정으로 설정하고 로드함
+    if (!setting.level) {
+        option = [{ office: 'dge.go.kr' }, { school: 'D100000282' }, { level: '4' }, { date: getYmd(new Date()) }]
+        // 설정
+        option.forEach(function (e, i) {
+            chrome.storage.local.set(e, function () {
+                // 모두 세팅을 마쳤을 때
+                if (i + 1 === option.length) {
+                    chrome.storage.local.get(keys, function (setting) {
+                        getMenu(getYmd(new Date()), setting)
+                    })
+                }
+            })
+        })
+    } else {
+        // get Menu
+        getMenu(getYmd(new Date()), setting)
+    }
+})
+
+// 급식 메뉴 가져오기
+function getMenu(ymd, setting) {
+    // 아침
+    $.get(`${baseURL}/api/meal/${ymd}/type/1/office/${setting.office}/school/${setting.school}/level/${setting.level}`, function (data) {
         let meal = JSON.parse(data)
         let breakfast = '<ul>'
         if (meal.menus) {
-
             meal.menus.forEach(element => {
                 breakfast += '<li>' + element + '</li>'
-            });
+            })
         }
         breakfast += '</ul>'
         if (breakfast === '<ul></ul>') {
-            breakfast = '메뉴가 없습니다.';
+            breakfast = '메뉴가 없습니다.'
         }
-        chrome.storage.local.set({breakfast: breakfast}, function () {
-            console.log('set localstorage data!');
-        });
+        chrome.storage.local.set({ breakfast: breakfast }, function () {
+            console.log('set localstorage data!')
+        })
     })
 
-    $.get(`http://jrady721.cafe24.com/api/meal/${ymd}/type/2/office/${data.office}/school/${data.school}/level/${data.level}`, function (data) {
+    // 점심
+    $.get(`${baseURL}/api/meal/${ymd}/type/2/office/${setting.office}/school/${setting.school}/level/${setting.level}`, function (data) {
         let meal = JSON.parse(data)
         let lunch = '<ul>'
         if (meal.menus) {
 
             meal.menus.forEach(element => {
                 lunch += '<li>' + element + '</li>'
-            });
+            })
         }
         lunch += '</ul>'
         if (lunch === '<ul></ul>') {
-            lunch = '메뉴가 없습니다.';
+            lunch = '메뉴가 없습니다.'
         }
-        chrome.storage.local.set({lunch: lunch}, function () {
-            console.log('set localstorage data!');
-        });
+        chrome.storage.local.set({ lunch: lunch }, function () {
+            console.log('set localstorage data!')
+        })
     })
 
-    $.get(`http://jrady721.cafe24.com/api/meal/${ymd}/type/3/office/${data.office}/school/${data.school}/level/${data.level}`, function (data) {
+    // 저녁
+    $.get(`${baseURL}/api/meal/${ymd}/type/3/office/${setting.office}/school/${setting.school}/level/${setting.level}`, function (data) {
         let meal = JSON.parse(data)
         let dinner = '<ul>'
         if (meal.menus) {
 
             meal.menus.forEach(element => {
                 dinner += '<li>' + element + '</li>'
-            });
+            })
         }
         dinner += '</ul>'
         if (dinner === '<ul></ul>') {
-            dinner = '메뉴가 없습니다.';
+            dinner = '메뉴가 없습니다.'
         }
-        chrome.storage.local.set({dinner: dinner}, function () {
-            console.log('set localstorage data!');
-        });
+        chrome.storage.local.set({ dinner: dinner }, function () {
+            console.log('set localstorage data!')
+        })
     })
 }
 
+// yyyy.mm.dd 포맷 가져오기
 function getYmd(date) {
-    let y = date.getFullYear().toString();
-    let m = (date.getMonth() + 1).toString();
-    let d = date.getDate().toString();
-    (d.length === 1) && (d = '0' + d);
-    (m.length === 1) && (m = '0' + m);
-    return y + '.' + m + '.' + d;
+    let y = date.getFullYear().toString()
+    let m = (date.getMonth() + 1).toString()
+    let d = date.getDate().toString()
+
+    if (d.length === 1) {
+        d = '0' + d
+    }
+    if (m.length === 1) {
+        m = '0' + m
+    }
+
+    return y + '.' + m + '.' + d
 }
--- public/js/script.js
+++ public/js/script.js
@@ -1,49 +1,61 @@
-let date = new Date();
-let keys = ['office', 'school', 'level'];
+const keys = ['office', 'school', 'level']
+const baseURL = 'http://jrady721.cafe24.com'
+
+let date = new Date()
 
 $(function () {
-    chrome.runtime.getBackgroundPage(e => {
-        $('#date').html(e.getYmd(new Date()));
+    // 설정된 오늘의 급식 불러오기
+    chrome.storage.local.get(['breakfast', 'lunch', 'dinner', 'date'], function (result) {
+        chrome.runtime.getBackgroundPage(e => {
+            // 만약 현재 날짜의 지금날짜가 동일하다면 로컬에서 불러오고 아니면 다시 로드한다.
+            if (result.date === e.getYmd(new Date())) {
+                $('#date').html(result.date)
+                $('#breakfast').html(result.breakfast)
+                $('#lunch').html(result.lunch)
+                $('#dinner').html(result.dinner)
+            } else {
+                $('#date').html(e.getYmd(new Date()))
+                getMenu(e.getYmd(new Date()))
+            }
+        })
     })
-    chrome.storage.local.get(['breakfast', 'lunch', 'dinner'], function (result) {
-        $('#breakfast').html(result.breakfast)
-        $('#lunch').html(result.lunch)
-        $('#dinner').html(result.dinner)
-    });
 
+    // 설정 불러오기
     chrome.storage.local.get(keys, function (data) {
-        $('#office').val(data.office);
-        $('#school').val(data.school);
-        $(`#level-${data.level}`).attr('checked', true);
+        $('#office').val(data.office)
+        $('#school').val(data.school)
+        $(`#level-${data.level}`).attr('checked', true)
     })
 })
 
+// 내일
 $('#btn-today').click(function () {
-    loading();
+    loading()
 
     chrome.runtime.getBackgroundPage(e => {
-        $('#date').html(e.getYmd(new Date()));
-        getMenu(e.getYmd(new Date()));
+        $('#date').html(e.getYmd(new Date()))
+        getMenu(e.getYmd(new Date()))
     })
 })
 
+// 오늘
 $('#btn-next').click(function () {
-    loading();
+    loading()
 
     chrome.runtime.getBackgroundPage(e => {
         date.setDate(date.getDate() + 1)
-        $('#date').html(e.getYmd(date));
+        $('#date').html(e.getYmd(date))
         getMenu(e.getYmd(date))
     })
 })
 
-
+// 이전날 
 $('#btn-prev').click(function () {
-    loading();
+    loading()
 
     chrome.runtime.getBackgroundPage(e => {
         date.setDate(date.getDate() - 1)
-        $('#date').html(e.getYmd(date));
+        $('#date').html(e.getYmd(date))
         getMenu(e.getYmd(date))
     })
 })
@@ -54,81 +66,84 @@ function loading() {
     $('#dinner').html('로딩중...')
 }
 
+// 급식 불러오기
 function getMenu(ymd) {
     chrome.storage.local.get(keys, function (data) {
-        $.get(`http://jrady721.cafe24.com/api/meal/${ymd}/type/1/office/${data.office}/school/${data.school}/level/${data.level}`, function (data) {
+        $.get(`${baseURL}/api/meal/${ymd}/type/1/office/${data.office}/school/${data.school}/level/${data.level}`, function (data) {
             let meal = JSON.parse(data)
             let breakfast = '<ul>'
             if (meal.menus) {
                 meal.menus.forEach(element => {
                     breakfast += '<li>' + element + '</li>'
-                });
+                })
             }
             breakfast += '</ul>'
             if (breakfast === '<ul></ul>') {
-                breakfast = '메뉴가 없습니다.';
+                breakfast = '메뉴가 없습니다.'
             }
             $('#breakfast').html(breakfast)
         })
 
-        $.get(`http://jrady721.cafe24.com/api/meal/${ymd}/type/2/office/${data.office}/school/${data.school}/level/${data.level}`, function (data) {
+        $.get(`${baseURL}/api/meal/${ymd}/type/2/office/${data.office}/school/${data.school}/level/${data.level}`, function (data) {
             let meal = JSON.parse(data)
             let lunch = '<ul>'
             if (meal.menus) {
                 meal.menus.forEach(element => {
                     lunch += '<li>' + element + '</li>'
-                });
+                })
             }
             lunch += '</ul>'
             if (lunch === '<ul></ul>') {
-                lunch = '메뉴가 없습니다.';
+                lunch = '메뉴가 없습니다.'
             }
             $('#lunch').html(lunch)
         })
 
-        $.get(`http://jrady721.cafe24.com/api/meal/${ymd}/type/3/office/${data.office}/school/${data.school}/level/${data.level}`, function (data) {
+        $.get(`${baseURL}/api/meal/${ymd}/type/3/office/${data.office}/school/${data.school}/level/${data.level}`, function (data) {
             let meal = JSON.parse(data)
             let dinner = '<ul>'
             if (meal.menus) {
                 meal.menus.forEach(element => {
                     dinner += '<li>' + element + '</li>'
-                });
+                })
             }
             dinner += '</ul>'
             if (dinner === '<ul></ul>') {
-                dinner = '메뉴가 없습니다.';
+                dinner = '메뉴가 없습니다.'
             }
             $('#dinner').html(dinner)
         })
     })
 }
-
+// setting save-btn
 $(document).on('click', '#ok', function () {
-    let office = $('#office').val();
-    let school = $('#school').val();
-    let level = $('input[name=level]:checked').val();
-    let setting = [{office: office}, {school: school}, {level: level}]
-    setting.forEach(function (data) {
-        chrome.storage.local.set(data, function () {
-            // console.log(data);
-        })
-    });
+    console.log('1')
+    let office = $('#office').val()
+    let school = $('#school').val()
+    let level = $('input[name=level]:checked').val()
+    let setting = [{ office: office }, { school: school }, { level: level }]
+
+    chrome.storage.local.set(setting, function () {
+        // console.log(data)
+    })
 
     chrome.storage.local.get(keys, function (data) {
         chrome.runtime.getBackgroundPage(e => {
-            e.getMenu(e.getYmd(new Date()), data);
+            e.getMenu(e.getYmd(new Date()), data)
 
-            alert('저장되었습니다!');
-            location.href = '../index.html';
+            alert('저장되었습니다!')
+            location.href = '../index.html'
         })
     })
-
 })
+
+// 취소
 $(document).on('click', '#cancel', function () {
-    location.href = '../index.html';
+    location.href = '../index.html'
 })
 
+// 학교 검색
 $(document).on('click', '#search', function () {
-    let url = 'https://www.meatwatch.go.kr/biz/bm/sel/schoolListPopup.do';
-    window.open(url, '학교코드 검색', 'height=400, width=500');
+    let url = 'https://www.meatwatch.go.kr/biz/bm/sel/schoolListPopup.do'
+    window.open(url, '학교코드 검색', 'height=400, width=500')
 })
